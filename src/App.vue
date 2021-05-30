fc6e1f7b671233ba4b4be80f7cf69dc45df7caf3
Piero Condor <piero.condor26@gmail.com>
2021-05-30 01:30:08 -0500

    instalacion de bootstrap y creacion de header

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <div style="height:52px"></div>
+    <Header/>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
+import Header from './components/header.vue'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
+    Header,
     HelloWorld
   }
 }
@@ -23,6 +26,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/header.vue
@@ -0,0 +1,40 @@
+<template>
+   <nav class="navbar navbar-expand-lg navbar-light fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" id="titulo" href="#">Ravn Star Wars Registry</a>
+    
+  </div>
+</nav>
+</template>
+
+
+<script>
+export default {
+  name: 'Header',
+ 
+}
+</script>
+<style>
+.navbar{
+    background-color: #121212;
+    height:52px;
+    padding:0px;
+}
+.container-fluid{
+    padding:0px;
+}
+#titulo{
+    color: #F2F2F2;
+    padding:0px;
+    margin-left:33px;
+    margin-top: 16px;
+    margin-bottom: 16px;
+    /*left:33px;*/
+    /*top:16px;*/
+    font-family: 'SF Pro Display';
+    font-style: normal;
+    font-weight: bold;
+    font-size:17px;
+    line-height:20px;
+}
+</style>
