b0893eef113ce897fba6aa6c331b42ae13d56570
Piero Condor <piero.condor26@gmail.com>
2021-05-30 22:50:01 -0500

    estilos al componente body

--- src/components/body.vue
+++ src/components/body.vue
@@ -1,17 +1,33 @@
 <template>
  
 <div id="second">
-    {{eyeColor}}<br>
-    {{hairColor}}<br>
-    {{skinColor}}<br>
-    {{birthYear}}<br>
-    {{vehicleConnection}}<br>
-  <!-- <li v-for="vehicle in vehicles_array" :key="vehicle">
-    {{ vehicle }}
-  </li>-->
-  <div v-for="vehicle in vehicles_array" :key="vehicle">
-    {{ vehicle }}
+  <div id="rec_1">
+    <p id="text_1">General Information</p>
   </div>
+  <div id="rec_2">
+    <label id="text_2">Eye Color</label>
+    <label id="data_2">{{eyeColor}}</label>
+  </div>
+  <div id="rec_3">
+    <label id="text_3">Hair Color</label>
+     <label id="data_3">{{hairColor}}</label>
+  </div>
+  <div id="rec_4">
+    <label id="text_4">Skin Color</label>
+     <label id="data_4">{{skinColor}}</label>
+  </div>
+  <div id="rec_5">
+    <label id="text_5">Birth Year</label>
+     <label id="data_5">{{birthYear}}</label>
+  </div>
+   <div id="rec_6">
+    <p id="text_6">Vehicles</p>
+  </div>
+  <template v-for="vehicle in vehicles_array">
+    <div class="rec_7" :key="vehicle">
+    <label class="text_7">{{vehicle}}</label>
+    </div>
+  </template>
 </div>
 </template>
 
@@ -64,5 +80,69 @@ export default {
 <style>
 #second {
     overflow: hidden; 
+}
+#rec_1, #rec_6{
+  width:890px;
+  height:60px;
+  border-style: solid;
+  border-width:1px;
+  border-color:red;
+}
+
+#rec_2, #rec_3, #rec_4, #rec_5{
+  width:890px;
+  height:49px;
+  border-style: solid;
+  border-width:1px;
+  border-color:red;
+}
+.rec_7{
+   width:890px;
+  height:49px;
+  border-style: solid;
+  border-width:1px;
+  border-color:red;
+}
+#text_1, #text_6{
+   size:17px;
+  height:20px;
+  color:#333333;
+  font-weight:bold;
+  margin-top:32px;
+  text-align:left;
+  margin-left:16px;
+}
+
+#text_2, #text_3, #text_4, #text_5{
+  size:17px;
+  height:20px;
+  color:#828282;
+  font-weight:bold;
+  margin-top:14px;
+  text-align:left;
+  margin-left:16px;
+  float:left;
+}
+
+.text_7{
+  size:17px;
+  height:20px;
+  color:#828282;
+  font-weight:bold;
+  margin-top:14px;
+  text-align:left;
+  margin-left:16px;
+  float:left;
+}
+
+#data_2, #data_3, #data_4, #data_5{
+   text-align:right;
+   float:right;
+   margin-right:16px;
+   margin-top:14px;
+   size:17px;
+   height:20px;
+   color:#333333;
+   font-weight:bold;
 }
 </style>
